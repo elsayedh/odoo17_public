b2b2ea767752fdc375b05c0be3df13793cfd5317
elsayedh <elsayedh@hotmail.com>
2024-12-04 12:15:24 +0300

    dd

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ DESTINATION=$1
 PORT=$2
 CHAT=$3
 # clone Odoo directory
-git clone --depth=1 https://github.com/pro-777/odoo-17-docker-compose/ $DESTINATION
+git clone --depth=1 https://github.com/elsayedh/odoo17_public/ $DESTINATION
 rm -rf $DESTINATION/.git
 # set permission
 mkdir -p $DESTINATION/postgresql
